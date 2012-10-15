7776475fbb3457b759bf880a3e162b1b69d4faf4
Dr Blinken <drblinken@gmail.com>
2012-10-16 00:47:50 +0200

    created exercise 01

---
+++ Kara06.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Kara06 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kara06 extends Kara
+{
+    public static final int MAX_LENGTH = 20;
+    int length = 1;
+    /**
+     * Act - do whatever the Kara06 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }  
+}
